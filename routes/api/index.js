5e0848e85dee2cc0a7d1cd4e87628e5e9ea182a9
Cartrell KIDD <cartrellk56@gmail.com>
2021-12-02 18:23:24 -0500

    updated api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,2 +1,15 @@
-const router = require('express').Router();
+var models  = require('../../models');
+var express = require('express');
+var router  = express.Router();
+var users = require('./users.js');
+var trips = require('./trips.js');
+var userTrips = require('./userTrips.js');
 
+
+
+router.use('/users', users);
+router.use('/trips', trips);
+router.use('/userTrips', userTrips);
+
+
+module.exports = router;
