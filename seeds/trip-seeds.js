cc40de5b15824bdb6c48810996d63e70b63d22a4
murphyoc1 <murphyoc1@gmail.com>
2021-12-05 20:19:01 -0500

    added css formating prior to changing the db for adding picture links

--- seeds/trip-seeds.js
+++ seeds/trip-seeds.js
@@ -2,25 +2,25 @@ const { Trip } = require('../models');
 
 const TripData = [
   {
-    title: "beach",
-    location: "bahamas",
+    title: "Sun Island All Inclusive Resort",
+    location: "Maldives",
     date: "December 15 to December 19, 2022",
     includes: "4 Nights/ 5 Days. Food and Drink",
     price: "$2,550"
   },
   {
-    title: "city",
-    location: "new york",
-    date: "December 15 to December 19, 2022",
+    title: "Donner MemorialState Park",
+    location: "California",
+    date: "July 24 to July 30, 2022",
     includes: "4 Nights/ 5 Days. Food and Drink",
     price: "$2,550"
   },
   {
-    title: "woods",
-    location: "the woods",
-    date: "December 15 to December 19, 2022",
-    includes: "4 Nights/ 5 Days. Food and Drink",
-    price: "$2,550"
+    title: "Masai Mara Safari",
+    location: "Nairobi, Kenya",
+    date: "September 19 to September 25, 2022",
+    includes: "6 Nights/ 7 Days. Food and Drink",
+    price: "$3,720"
   },
 ];
 
